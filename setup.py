62064c7e15f2ebce072363ced2fcb7394b18814f
Anthony Scopatz <scopatz@gmail.com>
2019-11-25 18:50:43 -0500

    intial setup.py

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import os
+import sys
+
+from setuptools import setup
+
+
+def main():
+    """The main entry point."""
+    with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
+        readme = f.read()
+    skw = dict(
+        name='runthis-sphinxext',
+        description='Provides a sphinx code-block for rendering RunThis blocks',
+        long_description=readme,
+        long_description_content_type='text/markdown',
+        license='BSD',
+        version='0.0.0',
+        author='Anthony Scopatz',
+        maintainer='Anthony Scopatz',
+        author_email='scopatz@gmail.com',
+        url='https://github.com/regro/runthis-sphinxext',
+        platforms='Cross Platform',
+        classifiers=['Programming Language :: Python :: 3'],
+        py_modules=['runthis_sphinxext'],
+        data_files=[],
+        install_requires=[],
+        python_requires=">=3.6",
+        zip_safe=False,
+        entry_points={
+            'sphinx.builders': [
+                'runthis = runthis_sphinxext',
+            ],
+        }
+    )
+    setup(**skw)
+
+
+if __name__ == '__main__':
+    main()
