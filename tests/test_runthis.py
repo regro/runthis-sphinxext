7341cf8f4235dea53d7f316ade80302f681b8dee
Anthony Scopatz <scopatz@gmail.com>
2019-11-25 22:27:05 -0500

    initial tests

---
+++ tests/test_runthis.py
@@ -0,0 +1,19 @@
+import os
+import subprocess
+
+import pytest
+
+
+def test_sphinxext():
+    # build docs first
+    cwd = os.path.dirname(__file__)
+    docs = os.path.join(cwd, 'docs')
+    subprocess.run(["make", "clean", "all"], check=True, cwd=docs)
+
+    # read the output
+    index = os.path.join(docs, "_build", "html", "index.html")
+    with open(index) as f:
+        s = f.read()
+
+    # check the output
+    assert "runthis" in s
