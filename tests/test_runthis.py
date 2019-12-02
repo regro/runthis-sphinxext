ce3b1134b24f47491e1e7042d06f90b94e21cf29
Anthony Scopatz <scopatz@gmail.com>
2019-12-02 17:04:40 -0500

    fix up test

--- tests/test_runthis.py
+++ tests/test_runthis.py
@@ -8,11 +8,9 @@ RT1 = r"""
 <script type="text/javascript">
   var app = Elm.Main.init({
     node: document.getElementById("runthis1"),
-    flags: {"placeholder": "<div class=\"highlight-python notranslate\"><div class=\"highlight\"><pre><span></span><span class=\"kn\">import</span> <span class=\"nn\">sys</span>\n<span class=\"k\">print</span><span class=\"p\">(</span><span class=\"n\">sys</span><span class=\"o\">.</span><span class=\"n\">executable</span><span class=\"p\">)</span>\n</pre></div>\n</div>\n", "presetup": "", "serverUrl": "http://localhost:5000", "setup": "import sys\nprint(sys.executable)\n"}
-  });
-</script>
-</div>
-"""
+    flags: {"placeholder": "<div class=
+""".strip()
+
 
 def test_sphinxext():
     # build docs first
