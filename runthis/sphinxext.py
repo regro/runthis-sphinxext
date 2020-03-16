50dedc22d80454d65842fe5f71b8aa37344a7295
Anthony Scopatz <scopatz@gmail.com>
2020-03-15 23:51:08 -0500

    bumped version to 0.0.2

--- runthis/sphinxext.py
+++ runthis/sphinxext.py
@@ -27,7 +27,7 @@ from docutils import nodes
 from docutils.parsers.rst import directives
 from sphinx.directives.code import CodeBlock
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 RT_COUNTER = 0
 JS_RUNTHIS = """\
 <div id="{divid}"></div>
