53a6156b4ee0d759b55f23b894a1a8a689986988
Anthony Scopatz <scopatz@gmail.com>
2020-03-17 10:12:41 -0500

    news and black

---
+++ news/setup-fix.md
@@ -0,0 +1,24 @@
+**Added:**
+
+* <news item>
+
+**Changed:**
+
+* <news item>
+
+**Deprecated:**
+
+* <news item>
+
+**Removed:**
+
+* <news item>
+
+**Fixed:**
+
+* Fixed setup.py to ensure that the proper version of the
+  RunThis client is actually used.
+
+**Security:**
+
+* <news item>
--- setup.py
+++ setup.py
@@ -11,23 +11,23 @@ CLIENT_VERSION = "0.0.3"
 
 def download_client():
     url = f"https://github.com/regro/runthis-client/releases/download/{CLIENT_VERSION}/runthis-client-{CLIENT_VERSION}.min.js"
-    client_fname = os.path.join('runthis', f'runthis-client-{CLIENT_VERSION}.min.js')
+    client_fname = os.path.join("runthis", f"runthis-client-{CLIENT_VERSION}.min.js")
     if not os.path.isfile(client_fname):
         with urllib.request.urlopen(url) as f:
             b = f.read()
-        with open(client_fname, 'wb') as f:
+        with open(client_fname, "wb") as f:
             f.write(b)
 
 
 def checksum_client():
     # download checksum
-    client_fname = os.path.join('runthis', f'runthis-client-{CLIENT_VERSION}.min.js')
+    client_fname = os.path.join("runthis", f"runthis-client-{CLIENT_VERSION}.min.js")
     url = f"https://github.com/regro/runthis-client/releases/download/{CLIENT_VERSION}/sha256.txt"
     sha256_fname = f"sha256-{CLIENT_VERSION}.txt"
     if not os.path.isfile(sha256_fname):
         with urllib.request.urlopen(url) as f:
             b = f.read()
-        with open(sha256_fname, 'wb') as f:
+        with open(sha256_fname, "wb") as f:
             f.write(b)
     # make checksum mapping
     with open(sha256_fname) as f:
@@ -35,24 +35,26 @@ def checksum_client():
     tups = [line.split() for line in data.splitlines()]
     sums = {fname: value for value, fname in tups}
     # get checksum
-    with open(client_fname, 'rb') as f:
+    with open(client_fname, "rb") as f:
         client = f.read()
     obs = hashlib.sha256(client).hexdigest()
-    assert sums[f"runthis-client-{CLIENT_VERSION}.min.js"] == obs, f"runthis-client-{CLIENT_VERSION}.min.js checksums don't match"
+    assert (
+        sums[f"runthis-client-{CLIENT_VERSION}.min.js"] == obs
+    ), f"runthis-client-{CLIENT_VERSION}.min.js checksums don't match"
 
 
 def copy_client():
-    src = os.path.join('runthis', f'runthis-client-{CLIENT_VERSION}.min.js')
-    tar = os.path.join('runthis', f'runthis-client.min.js')
-    with open(src, 'rb') as f:
+    src = os.path.join("runthis", f"runthis-client-{CLIENT_VERSION}.min.js")
+    tar = os.path.join("runthis", f"runthis-client.min.js")
+    with open(src, "rb") as f:
         b = f.read()
-    with open(tar, 'wb') as f:
+    with open(tar, "wb") as f:
         f.write(b)
 
 
 def main():
     """The main entry point."""
-    with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
+    with open(os.path.join(os.path.dirname(__file__), "README.md"), "r") as f:
         readme = f.read()
     # make sure we have the client with the correct version avialable
     download_client()
@@ -60,33 +62,29 @@ def main():
     copy_client()
     # actually install
     skw = dict(
-        name='runthis-sphinxext',
-        description='Provides a sphinx code-block for rendering RunThis blocks',
+        name="runthis-sphinxext",
+        description="Provides a sphinx code-block for rendering RunThis blocks",
         long_description=readme,
-        long_description_content_type='text/markdown',
-        license='BSD',
-        version='0.0.2',
-        author='Anthony Scopatz',
-        maintainer='Anthony Scopatz',
-        author_email='scopatz@gmail.com',
-        url='https://github.com/regro/runthis-sphinxext',
-        platforms='Cross Platform',
-        classifiers=['Programming Language :: Python :: 3'],
-        py_modules=['runthis_sphinxext'],
-        packages=['runthis'],
-        package_dir={'runthis': 'runthis'},
-        package_data={'runthis': ['runthis-client.min.js']},
+        long_description_content_type="text/markdown",
+        license="BSD",
+        version="0.0.2",
+        author="Anthony Scopatz",
+        maintainer="Anthony Scopatz",
+        author_email="scopatz@gmail.com",
+        url="https://github.com/regro/runthis-sphinxext",
+        platforms="Cross Platform",
+        classifiers=["Programming Language :: Python :: 3"],
+        py_modules=["runthis_sphinxext"],
+        packages=["runthis"],
+        package_dir={"runthis": "runthis"},
+        package_data={"runthis": ["runthis-client.min.js"]},
         install_requires=[],
         python_requires=">=3.6",
         zip_safe=False,
-        entry_points={
-            'sphinx.builders': [
-                'runthis = runthis.sphinxext',
-            ],
-        }
+        entry_points={"sphinx.builders": ["runthis = runthis.sphinxext",],},
     )
     setup(**skw)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
